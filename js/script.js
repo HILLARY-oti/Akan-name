6fdf4264b73f3e60ac045372d00029d52ded776f
HILLARY-oti <hillaryotieno646@gmail.com>
2019-12-01 23:08:38 +0300

    add arrays to js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var century, year, month, dayOfMonth, dayOfWeek, day;
+//arrays
+let daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
