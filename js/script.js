b0ab087a7d2952013d46dfe94b161c0702419d3c
HILLARY-oti <hillaryotieno646@gmail.com>
2019-12-01 23:10:13 +0300

    add inputs

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ var century, year, month, dayOfMonth, dayOfWeek, day;
 //arrays
 let daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+//Get input
+function getInput() {
+  century = parseInt(document.getElementById("century").value);
+  year = parseInt(document.getElementById("year").value);
+  month = parseInt(document.getElementById("month").value);
+  dayOfMonth = parseInt(document.getElementById("dayOfMonth").value);
+}
